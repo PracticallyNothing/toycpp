3b77d35d4557f0b2da6121620167144a801e481e
PracticallyNothing <practicallynothingatall@gmail.com>
2025-09-26 12:07:31 +0300

    [Fix] color.hpp: Whoops, green and yellow were flipped
    Whoops, green is actually ANSI code 32 and yellow is 33, but I had them flipped around.

--- src/color.hpp
+++ src/color.hpp
@@ -6,14 +6,10 @@ using std::string;
 inline string bold(const string &s) { return "\033[1m" + s + "\033[0m"; }
 
 inline string boldred(const string &s) { return "\033[31;1m" + s + "\033[0m"; }
-inline string boldyellow(const string &s) {
-  return "\033[32;1m" + s + "\033[0m";
-}
-inline string boldgreen(const string &s) {
-  return "\033[33;1m" + s + "\033[0m";
-}
+inline string boldyellow(const string &s) { return "\033[33;1m" + s + "\033[0m"; }
+inline string boldgreen(const string &s) { return "\033[32;1m" + s + "\033[0m"; }
 
 inline string red(const string &s) { return "\033[31m" + s + "\033[0m"; }
-inline string yellow(const string &s) { return "\033[32m" + s + "\033[0m"; }
-inline string green(const string &s) { return "\033[33m" + s + "\033[0m"; }
+inline string yellow(const string &s) { return "\033[33m" + s + "\033[0m"; }
+inline string green(const string &s) { return "\033[32m" + s + "\033[0m"; }
 } // namespace color
